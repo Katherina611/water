e8145b8efc88c21da6cd21dfa9235b7dd30a1f89
Katherina611 <katarzynag0609@gmail.com>
2018-07-10 14:46:13 +0200

    class calc

--- js/app.jsx
+++ js/app.jsx
@@ -1,1 +1,53 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./../scss/style.scss";
+import {
+    Router,
+    Route,
+    Link,
+    IndexLink,
+    IndexRoute,
+    hashHistory
+} from 'react-router';
+
+document.addEventListener('DOMContentLoaded', function() {
+    class Calc extends React.Component {
+        render() {
+            return (
+                <form>
+                    <fieldset>Obliczenia chloramin<br></br>
+                        <label>
+                            <input type="number" id="c1" placeholder="Podaj absorbancje 1"/>
+                            <input type="number" id="c1-2" placeholder="Podaj absorbancje 1'"/>
+                        </label><br></br>
+                        <label>
+                            <input type="number" id="c2" placeholder="Podaj absorbancje 2"/>
+                            <input type="number" id="c2-2" placeholder="Podaj absorbancje 2'"/>
+                        </label><br></br>
+                        <label>
+                            <input type="number" id="c4" placeholder="Podaj absorbancje 3"/>
+                            <input type="number" id="c4-2" placeholder="Podaj absorbancje 3'"/>
+                        </label><br></br>
+                        <label>
+                            <input type="number" id="c5" placeholder="Podaj absorbancje 4"/>
+                            <input type="number" id="c5-2" placeholder="Podaj absorbancje 4'"/>
+                        </label><br></br>
+                        <label>
+                            <input type="submit" id="result" value="Oblicz"/>
+                        </label>
+                        <div class="resultDiv"></div>
+                    </fieldset>
+                </form>
+            )
+        }
+    }
+
+    ReactDOM.render(
+        <Calc/>,
+        document.getElementById('app')
+    )
+
+
+});
+
+
